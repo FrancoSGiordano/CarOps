9f24f6b81fa7211070f8e2524f2291123f91d689
IvanLapegna <lapegnaivan@gmail.com>
2025-10-25 19:24:26 -0300

    minimo de recordatorios, a medias

--- app/src/main/java/com/example/trabajointegrador_modulonativo/carDetailFragment.kt
+++ app/src/main/java/com/example/trabajointegrador_modulonativo/carDetailFragment.kt
@@ -82,6 +82,16 @@ class carDetailFragment : Fragment() {
                 Toast.makeText(context, "Cargando datos del vehículo...", Toast.LENGTH_SHORT).show()
             }
         }
+
+        binding.viewAlarmButton?.setOnClickListener {
+            val carId = viewModel.selectedCar.value?.id
+            if (carId != null) {
+                val bundle = Bundle().apply { putString("car_id", carId) }
+                findNavController().navigate(R.id.action_detail_to_alarms, bundle)
+            } else {
+                Toast.makeText(context, "Cargando datos del vehículo...", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun observeViewModel() {
