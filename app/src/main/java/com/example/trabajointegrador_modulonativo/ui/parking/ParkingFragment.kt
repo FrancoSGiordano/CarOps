ab952408be9e7b79be4a44a89f9faad13da6fb56
IvanLapegna <lapegnaivan@gmail.com>
2025-10-26 15:45:01 -0300

    intento de parking

---
+++ app/src/main/java/com/example/trabajointegrador_modulonativo/ui/parking/ParkingFragment.kt
@@ -0,0 +1,22 @@
+package com.example.trabajointegrador_modulonativo.ui.parking
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.trabajointegrador_modulonativo.databinding.FragmentParkingBinding
+
+class ParkingFragment: Fragment() {
+    private var _binding: FragmentParkingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentParkingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
